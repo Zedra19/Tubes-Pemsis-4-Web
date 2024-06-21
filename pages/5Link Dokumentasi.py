2cc41427b51c8ab6bcc992222fef046f7ec941d5
Zedra19 <zedra1911@gmail.com>
2024-06-21 23:58:01 +0700

    menyediakan link untuk dokumentasi

---
+++ pages/5Link Dokumentasi.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Link Dokumentasi", page_icon="📈")
+
+st.header("Link Google Collab")
+st.markdown("""[https://colab.research.google.com/drive/1bEBzT8pdzeO8ljSKr0wcgzcv4OOpGcB6?usp=sharing](https://colab.research.google.com/drive/1bEBzT8pdzeO8ljSKr0wcgzcv4OOpGcB6?usp=sharing)""")
+
+st.header("Link Laporan Tugas Besar")
+st.markdown("""""")
+
+st.header("Link Youtube Video")
+st.markdown("""""")
